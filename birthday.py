f8fc5c850b4213048588d0de30c713b50b026010
avianajoshi <rajesh.joshi1986@gmail.com>
2026-06-18 19:14:26 +0100

    birthdays

---
+++ birthday.py
@@ -0,0 +1,19 @@
+print("Birthdays")
+
+ashvetha = "24th January 2017"
+print("Ashvetha's birthday is on,",(ashvetha))
+
+prisha = "17th August 2017"
+print("Prisha's birthday is on,",(prisha))
+
+isha = "12th August 2017"
+print("Isha's birthday is on,",(isha))
+
+jensi = "19th July 2017"
+print("Jensi's birthday is on,",(jensi))
+
+navisha = "7th May 2017"
+print("Navisha's birthday is on,",(navisha))
+
+dhiya = "25th July 2017"
+print("Dhiya's birthday is on,",(dhiya))
