89cbad6c3960acd670c3a36e52a10c83070c0f15
avianajoshi <rajesh.joshi1986@gmail.com>
2026-06-22 17:42:45 +0100

    operators1

---
+++ percent.py
@@ -0,0 +1,13 @@
+print("Enter Marcks Obstained in 4 subjects: ")
+math = int(input("Maths :"))
+english = int(input("English :"))
+science = int(input("Science :"))
+hindi = int(input("Hindi :"))
+
+sum = math+english+science+hindi
+print("Sum of math, english, science and hindi = ",sum)
+
+perc = (sum/400)*100
+
+print(end= "Percentage Mrk = ")
+print(perc)
