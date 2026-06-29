b0d5b7543f08e885cf313fb9e29de28ca6f5382e
avianajoshi <rajesh.joshi1986@gmail.com>
2026-06-29 17:43:35 +0100

    conditional statements

---
+++ greaterorsmaller.py
@@ -0,0 +1,9 @@
+i = int(input("Please enter a number."))
+if (i < 15):
+    print("i is in the if BLOCK")
+    print("i is less than 15")
+else:
+    print("i is in the else statement")
+    print("i is greater than 15")
+print("i is not in the if statement or the else statement.")
+    
