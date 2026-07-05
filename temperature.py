79069e78a3f6ef7e3b55a0c057d769fae2b3a4ac
avianajoshi <rajesh.joshi1986@gmail.com>
2026-07-05 17:02:03 +0100

    Temperature

---
+++ temperature.py
@@ -0,0 +1,7 @@
+temperature = input("How is the weather, hot or cold?")
+
+if (temperature == "hot") or (temperature == "Hot"):
+    print("I suggest you to wear light clothes like T-shirt and shorts or a dress if you are a girl.")
+
+if (temperature == "cold") or (temperature == "Cold"):
+    print("You should wear thick clothes like a jacket or you might catch a cold or a fever.")
