bbefa194f6f338ee89d2f5d92b70fb75377b9bac
avianajoshi <rajesh.joshi1986@gmail.com>
2026-07-13 17:30:31 +0100

    Identity

---
+++ identity.py
@@ -0,0 +1,34 @@
+x = 5
+
+if (type(x) is int):
+
+    print("True")
+
+else:
+
+    print("False")
+
+ x = 5.5
+
+if (type(x)is not float):
+
+    print("True")
+
+x = 20
+
+y = 20
+
+if (x is not y):
+
+    print("x & y have SAME identity")
+y = 30
+
+if (x is not y):
+
+    print("x & y have DIFFERENT identity")
+
+
+
+
+
+
